e1e046af780db292dd419b96f12dc5480c75d004
charangangisetti <charangangisetti@gmail.com>
2025-06-06 01:25:05 +0530

    init

--- src/ex_02_Java_Basic/Lab011_HQ.java
+++ src/ex_02_Java_Basic/Lab011_HQ.java
@@ -2,6 +2,6 @@ package ex_02_Java_Basic;
 
 public class Lab011_HQ {
     public static void main(String[] args) {
-        System.out.println("CHARAN");
+        System.out.println("CHARAN G");
     }
 }
