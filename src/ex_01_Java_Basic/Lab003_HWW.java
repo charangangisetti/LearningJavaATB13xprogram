920340da1958d9c913e6b7ac634fcec877aaaeca
charangangisetti <charangangisetti@gmail.com>
2025-06-06 00:19:57 +0530

    init commit

--- src/ex_01_Java_Basic/Lab003_HWW.java
+++ src/ex_01_Java_Basic/Lab003_HWW.java
@@ -1,7 +1,7 @@
 package ex_01_Java_Basic;
 
-public class Lab002_HW_EXTRA {
+public class Lab003_HWW {
     public static void main(String[] args) {
-        System.out.println("2+2");
+        System.out.println(5+5);
     }
 }
--- src/ex_02_Java_Basic/Lab011_HQ.java
+++ src/ex_02_Java_Basic/Lab011_HQ.java
@@ -1,6 +1,6 @@
 package ex_02_Java_Basic;
 
-public class Lab001_HQ {
+public class Lab011_HQ {
     public static void main(String[] args) {
         System.out.println("CHARAN");
     }
