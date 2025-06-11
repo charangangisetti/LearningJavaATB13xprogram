e47556799f3ed3061171a92add93f4eafdf64691
charangangisetti <charangangisetti@gmail.com>
2025-06-12 00:30:15 +0530

    10th June 2025

---
+++ src/ex_02_Java_Basic/Lab020_Interview_QnA2.java
@@ -0,0 +1,12 @@
+package ex_02_Java_Basic;
+
+public class Lab020_Interview_QnA2 {
+    public static void main(String[] args) {
+//      int enum = 10;(Because enum is a reserved keyword in Java since Java 5 (when enums were introduced). You cannot use it as a variable name, class name, method name, etc.)
+        int Enum = 10;
+        int a_b = 89;
+//      int charan gangisetti = 89;(You cannot have a space in a variable name)
+        int charan$gangisetti = 90;
+        int Lab013 = 123;
+    }
+}
