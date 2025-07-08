828d1731f36226d1f014831e2bc43afec79969c7
charangangisetti <charangangisetti@gmail.com>
2025-07-09 00:51:53 +0530

    12th June 2025

---
+++ src/ex_05_TypeCasting/Lab059_TypeCasting_Narrowing.java
@@ -0,0 +1,12 @@
+package ex_05_TypeCasting;
+
+public class Lab059_TypeCasting_Narrowing {
+    public static void main(String[] args) {
+        int val = 300;
+//        byte b = val; // Narrowing (int -> byte) - Implicit Casting is not allowed.
+        byte b1 = (byte)val;
+        // Data Loss.
+        System.out.println(b1);// Narrowing (int -> byte) - Explicit Casting is not
+
+    }
+}
